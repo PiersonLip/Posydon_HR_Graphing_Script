0997dc571a420609b476f4c13f25aa2632c7cfc8
Pierson Lipschultz <piersonlipschultz@gmail.com>
2025-11-16 21:16:27 -0600

    added troubleshooting code

--- POSYDONHRDiagramModule.py
+++ POSYDONHRDiagramModule.py
@@ -234,6 +234,10 @@ def HR_Diagram     (df,  # Pandas dataframe to used (or H5). however, it is recc
             plt.show()
         plt.close()
 
+        print(df.columns)
+        print(S2_log_L, S2_log_R, variable)
+        print(np.isfinite(Temp).sum(), np.isfinite(Lum).sum(), np.isfinite(color_data).sum())
+
 
 
     ### Bokeh Graphing
